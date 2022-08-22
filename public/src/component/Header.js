3627de3ab7d129f8345c410da8b62056ead96c67
dee0518 <dehya-518@naver.com>
2022-08-22 15:11:59 +0900

    :memo: header active 클래스 추가 및 구조 변경

--- public/src/component/Header.js
+++ public/src/component/Header.js
@@ -4,15 +4,27 @@ import { NewTravelLogModal } from './index.js';
 
 class Header extends Component {
   render() {
+    const path = window.location.pathname;
     const newTravelLogModal = new NewTravelLogModal().render();
+    const navList = [
+      { href: '/main', content: '여행일지' },
+      { href: '#', content: '일정 만들기' },
+      { href: '/login', content: '로그인' },
+    ];
+
+    // prettier-ignore
     return `
       <header class="header">
         <nav class="nav">
           <h1 class="logo"><img src="./assets/images/HelloWorldLogo.svg" alt="Hello World" /></h1>
           <ul class="nav__list">
-            <li class="nav__item"><a href="#" class="nav__item__link">여행일지</a></li>
-            <li class="nav__item"><a href="#" class="nav__item__link showNewTravelLogModal">일정 만들기</a></li>
-            <li class="nav__item"><a href="#" class="nav__item__link">로그인</a></li>
+           ${navList.map(({ href, content }) => {
+            if(path === '/' && href !== '/login') return '';
+
+            return `<li class="nav__item ${path === href ? 'active' : ''}">
+            <a href="${href}" class="nav__item__link">${content}</a>
+            </li>`
+           }).join('')}
           </ul>
         </nav>
       </header>
@@ -22,13 +34,22 @@ class Header extends Component {
     `;
   }
 
-  showNewTravelLogModal(e) {
+  link(e) {
+    if (!e.target.matches('.nav__item__link')) return;
+
     e.preventDefault();
-    document.querySelector('.newTravelLogModal .dimmed__layer').classList.remove('hide');
+    const path = e.target.getAttribute('href');
+
+    if (path === '#') {
+      document.querySelector('.newTravelLogModal .dimmed__layer').classList.remove('hide');
+    } else {
+      window.history.pushState({}, path, window.location.origin + path);
+      render();
+    }
   }
 
   addEventListener() {
-    return [{ type: 'click', selector: '.showNewTravelLogModal', handler: this.showNewTravelLogModal }];
+    return [{ type: 'click', selector: '.nav__list', handler: this.link }];
   }
 }
 
