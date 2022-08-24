8edb04063c222b100b0fa00d0c865f7fd29a1fc6
choi1five <99406837+choi1five@users.noreply.github.com>
2022-08-24 21:52:05 +0900

    :art: Edit & View directory  index.js route modify

--- public/src/store/store.js
+++ public/src/store/store.js
@@ -94,29 +94,6 @@ const store = {
             day: 'Sun',
             cells: [
               {
-                id: 5,
-                type: '',
-                startTime: '오후 10:00',
-                endTime: '오후 12:00',
-                location: '숙소',
-                memo: '',
-                todos: [],
-                article: {
-                  picture:
-                    'https://images.unsplash.com/photo-1569154941061-e231b4725ef1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-                  content:
-                    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati autem officia mollitia vel Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati autem officia mollitia vel Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati autem officia mollitia vel',
-                },
-              },
-            ],
-          },
-          {
-            id: 2,
-            country: '프랑스',
-            date: '08.15',
-            day: 'Sun',
-            cells: [
-              {
                 type: '',
                 startTime: '오후 08:00',
                 endTime: '오후 10:00',
